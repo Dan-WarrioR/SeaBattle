a24975f9eaf896f899c3b7a4d99c3f77ed378307
DanWarrioR <norkinDanya2006@gmail.com>
2024-11-17 19:16:07 +0100

    Writed custom vector2 for int values

---
+++ Source/Tools/Math/Vector2.cs
@@ -0,0 +1,32 @@
+﻿namespace Source.Tools.Math
+{
+    public struct Vector2
+    {
+        public static Vector2 Zero => new(0, 0);
+        public static Vector2 One => new(1, 1);
+
+        public int X;
+        public int Y;
+
+        public Vector2(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+		public static Vector2 operator +(Vector2 first, Vector2 second)
+		{
+			return new Vector2(first.X + second.X, first.Y + second.Y);
+		}
+
+		public static Vector2 operator -(Vector2 first, Vector2 second)
+		{
+			return new Vector2(first.X - second.X, first.Y - second.Y);
+		}
+
+		public static Vector2 operator +(Vector2 first, (int X, int Y) tuple)
+		{
+			return new Vector2(first.X + tuple.X, first.Y + tuple.Y);
+		}
+	}
+}
