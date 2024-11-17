0ec37a52243804963f53e87cfc67bb0bc76cdc4a
DanWarrioR <norkinDanya2006@gmail.com>
2024-11-17 19:19:59 +0100

    Fixed bugs in Game class with separated logic

--- Source/Game.cs
+++ Source/Game.cs
@@ -1,38 +1,44 @@
-﻿namespace Source
+﻿using Source.Input;
+using Source.MapGeneration;
+using Source.Rendering;
+using Source.Tools.Math;
+
+namespace Source
 {
 	public class Game
 	{
-		//Configs
-		private const ConsoleColor PlayerCursorColor = ConsoleColor.Green;
-		private const ConsoleColor DestroyedShipColor = ConsoleColor.Red;
-		private const ConsoleColor MissShotColor = ConsoleColor.Gray;
-		private const ConsoleColor SeaColor = ConsoleColor.Gray;
-		private const ConsoleColor ShipColor = ConsoleColor.DarkBlue;
-
+		private enum PlayerMove
+		{
+			Player,
+			Enemy,
+		}
+		
 		private const int MapSize = 10;
-		private const int SpaceBetweenMaps = 15;
+		
+		private Map _playerMap;
+		private Map _enemyMap;
 
-		private const char PlayerCursorIcon = 'o';
-		private const char SeaIcon = '.';
-		private const char ShipIcon = '⌂';
-		private const char DestroyedShipIcon = 'X';
-		private const char MissShotIcon = '#';
+		private PlayerMove _curentPlayerMove = PlayerMove.Player;
 
-		private const int ShipFrequency = 10;
+		private Renderer _renderer;
 
+		private PlayerInput _playerInput;
+		private PlayerInput _enemyInput;
 
+		public Game()
+		{
+			_playerMap = new(MapSize);
+			_playerMap.OnCellBombed += OnEnemyBombedCell;
 
-		private char[,] _playerMap = new char[MapSize, MapSize];
-		private char[,] _enemyMap = new char[MapSize, MapSize];
+			_enemyMap = new(MapSize);
+			_enemyMap.OnCellBombed += OnPlayerBombedCell;
 
-		private int _playerShipsCount = 0;
-		private int _enemyShipsCount = 0;
+			_renderer = new(_playerMap, _enemyMap);
+			_playerInput = new(_playerMap, Vector2.Zero);
+			_enemyInput = new(_enemyMap, Vector2.Zero);
+		}
 
-		private Random _random = new();
-
-		private (int x, int y) _playerPosition = (0, 0);
-
-		private bool _isMoveApproved = false;
+		//////////
 
 		public void Start()
 		{
@@ -51,332 +57,88 @@
 		{
 			do
 			{
-				GenerateMaps();
-
 				Draw();
 
 				while (!IsEndGame())
 				{
-					CalculatePlayerInput();
+					CalculateInput();
 
-					Logic();
+					DoLogic();
 
 					Draw();
 				}
 
-				DrawEndGameText();
-
-				Console.WriteLine("Play again? (Y / N)");
+				DrawEndGameText();		
 			}
 			while (Console.ReadKey(true).Key == ConsoleKey.Y);
-		}
+		}	
 
-		//Player Input
-
-		private void CalculatePlayerInput()
+		private void CalculateInput()
 		{
-			var key = Console.ReadKey(true).Key;
-
-			GetPlayerMove(key);
-
-			GetPlayerCommand(key);
-		}
-
-		private void GetPlayerMove(ConsoleKey key)
-		{
-			(int x, int y) delta = key switch
+			if (_curentPlayerMove == PlayerMove.Player)
 			{
-				ConsoleKey.UpArrow or ConsoleKey.W => (0, -1),
-				ConsoleKey.DownArrow or ConsoleKey.S => (0, 1),
-				ConsoleKey.LeftArrow or ConsoleKey.A => (-1, 0),
-				ConsoleKey.RightArrow or ConsoleKey.D => (1, 0),
-				_ => (0, 0),
-			};
-
-			if (IsInBorders(_playerPosition.x + delta.x, _playerPosition.y + delta.y))
-			{
-				_playerPosition.x += delta.x;
-				_playerPosition.y += delta.y;
-			}
-		}
-
-		private void GetPlayerCommand(ConsoleKey key)
-		{
-			switch (key)
-			{
-				case ConsoleKey.Enter:
-					_isMoveApproved = true;
-					break;
-				default:
-					break;
-			}
-		}
-
-		private bool IsInBorders(int x, int y)
-		{
-			return x >= 0 && x < MapSize && y >= 0 && y < MapSize;
-		}
-
-		//Map Generation
-		private void GenerateMaps()
-		{
-			_playerMap = new char[MapSize, MapSize];
-			_enemyMap = new char[MapSize, MapSize];
-
-			for (int i = 0; i < MapSize; i++)
-			{
-				for (int j = 0; j < MapSize; j++)
-				{
-					_enemyMap[i, j] = GenerateCell(ref _enemyShipsCount);
-					_playerMap[i, j] = GenerateCell(ref _playerShipsCount);
-				}
-			}
-		}
-
-		private char GenerateCell(ref int shipCount)
-		{
-			if (_random.Next(0, 100) < ShipFrequency)
-			{
-				shipCount++;
-
-				return ShipIcon;
-			}
-
-			return SeaIcon;
-		}
-
-		//Game Logic
-
-		private void Logic()
-		{
-			if (!CanDoLogic())
-			{
-				return;
-			}
-
-			TryPlayerShoot(_playerPosition.x, _playerPosition.y);
-
-			TryEnemyShoot();
-		}
-
-		private void TryPlayerShoot(int x, int y)
-		{
-			if (!CanPlayerDoShoot(x, y))
-			{
-				return;
-			}
-
-			DoPlayerShoot(x, y);
-		}
-
-		private bool CanPlayerDoShoot(int x, int y)
-		{
-			if (!IsValidMove(x, y, _enemyMap))
-			{
-				return false;
-			}
-
-			if (_enemyMap[x, y] == DestroyedShipIcon)
-			{
-				return false;
-			}
-
-			return true;
-		}
-
-		private void DoPlayerShoot(int x, int y)
-		{
-			if (_enemyMap[x, y] == ShipIcon)
-			{
-				_enemyMap[x, y] = DestroyedShipIcon;
-
-				_enemyShipsCount--;
+				_playerInput.UpdatePlayerInput();
 			}
 			else
 			{
-				_enemyMap[x, y] = MissShotIcon;
+				_enemyInput.UpdatePlayerInput(true);
 			}
-
-			_isMoveApproved = false;
 		}
 
-		private void TryEnemyShoot()
+		private void DoLogic()
 		{
-			if (!CanEnemyShoot())
+			if (_curentPlayerMove == PlayerMove.Player)
 			{
-				return;
-			}
+				if (!_playerInput.IsConfirmed)
+				{
+					return;
+				}
 
-			DoEnemyShoot();
-		}
-
-		private bool CanEnemyShoot()
-		{
-			return !_isMoveApproved;
-		}
-
-		private void DoEnemyShoot()
-		{
-			(int x, int y) = GetEnemyShootPosition();
-
-			if (_playerMap[x, y] == ShipIcon)
-			{
-				_playerMap[x, y] = DestroyedShipIcon;
-
-				_playerShipsCount--;
+				if (_enemyMap.IsValidMove(_playerInput.CurrentPosition.X, _playerInput.CurrentPosition.Y) && !_enemyMap.TryBombCell(_playerInput.CurrentPosition))
+				{
+					_curentPlayerMove = PlayerMove.Enemy;
+				}
 			}
 			else
 			{
-				_playerMap[x, y] = MissShotIcon;
-			}
-		}
-
-		private (int x, int y) GetEnemyShootPosition()
-		{
-			List<(int x, int y)> freeCells = new();
-
-			for (int i = 0; i < MapSize; i++)
-			{
-				for (int j = 0; j < MapSize; j++)
+				if (!_playerMap.TryBombCell(_enemyInput.CurrentPosition))
 				{
-					if (IsValidMove(j, i, _playerMap))
-					{
-						freeCells.Add((j, i));
-					}
+					_curentPlayerMove = PlayerMove.Player;
 				}
 			}
-
-			return freeCells[_random.Next(0, freeCells.Count)];
 		}
-
-		//Game Conditions Checkers
 
 		private bool IsEndGame()
 		{
-			return _playerShipsCount <= 0 || _enemyShipsCount <= 0;
+			return _enemyMap.ShipsCount <= 0 || _playerMap.ShipsCount <= 0;
 		}
 
-		private bool CanDoLogic()
+		private void OnPlayerBombedCell(CellState state)
 		{
-			return _isMoveApproved;
-		}
-
-		private bool IsValidMove(int x, int y, char[,] map)
-		{
-			if (map[x, y] == DestroyedShipIcon || map[x, y] == MissShotIcon)
+			if (state == CellState.Miss)
 			{
-				return false;
+				_curentPlayerMove = PlayerMove.Enemy;
 			}
+		}
 
-			return true;
-		}	
+		private void OnEnemyBombedCell(CellState state)
+		{
+			if (state == CellState.Miss)
+			{
+				_curentPlayerMove = PlayerMove.Player;
+			}
+		}
 
-
-		//Frontend
-
+		//////////
 
 		private void Draw()
 		{
-			DrawMaps();
-
-			DrawPlayerCursor();
-
-			DrawStats();
-		}
-
-		private void DrawMaps()
-		{
-			Console.Clear();
-
-			for (int i = 0; i < MapSize; i++)
-			{
-				for (int j = 0; j < MapSize; j++)
-				{
-					DrawEnemyMapCell(j, i);
-
-					DrawPlayerMapCell(j, i);
-				}	
-
-				Console.WriteLine();
-			}
-		}
-
-		private void DrawEnemyMapCell(int x, int y)
-		{
-			char cellIcon = _enemyMap[x, y] == ShipIcon ? SeaIcon : _enemyMap[x, y];
-
-			ConsoleColor consoleColor = _enemyMap[x, y] switch
-			{
-				MissShotIcon => MissShotColor,
-				DestroyedShipIcon => DestroyedShipColor,
-				_ => SeaColor,
-			};
-
-			WriteColoredSymbol(consoleColor, cellIcon);
-		}
-
-		private void DrawPlayerMapCell(int x, int y)
-		{
-			ConsoleColor consoleColor = _playerMap[x, y] switch
-			{
-				MissShotIcon => MissShotColor,
-				DestroyedShipIcon => DestroyedShipColor,
-				ShipIcon => ShipColor,
-				_ => SeaColor,
-			};
-
-			WriteSymbolAtPlace(x + SpaceBetweenMaps + MapSize, y, consoleColor, _playerMap[x, y]);
-		}
-
-		private void DrawPlayerCursor()
-		{
-			WriteSymbolAtPlace(_playerPosition.x, _playerPosition.y, PlayerCursorColor, PlayerCursorIcon);
-		}
-
-		private void DrawStats()
-		{
-			Console.WriteLine($"\nEnemy ships - {_enemyShipsCount} | My ships - {_playerShipsCount}");
+			_renderer.Draw(_playerInput.CurrentPosition);
 		}
 
 		private void DrawEndGameText()
 		{
-			if (_playerShipsCount <= 0)
-			{
-				Console.WriteLine("You Lose!");
-			}
-			else if (_enemyShipsCount <= 0)
-			{
-				Console.WriteLine("You Won!");
-			}
-		}
-
-
-		//Help Methods
-
-
-		private void WriteSymbolAtPlace(int x, int y, char symbol)
-		{
-			WriteSymbolAtPlace(x, y, Console.ForegroundColor, symbol);
-		}
-
-		private void WriteSymbolAtPlace(int x, int y, ConsoleColor color, char symbol)
-		{
-			var cursorPosition = Console.GetCursorPosition();
-
-			Console.SetCursorPosition(x, y);
-
-			WriteColoredSymbol(color, symbol);
-
-			Console.SetCursorPosition(cursorPosition.Left, cursorPosition.Top);
-		}
-
-		private void WriteColoredSymbol(ConsoleColor color, char symbol)
-		{
-			var currentColor = Console.ForegroundColor;
-
-			Console.ForegroundColor = color;
-			Console.Write(symbol);
-			Console.ForegroundColor = currentColor;
+			_renderer.DrawEndGameText(_playerMap.ShipsCount > 0);
 		}
 	}
 }
