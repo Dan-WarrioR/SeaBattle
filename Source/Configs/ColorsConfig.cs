8f8e6d522003c80806c6fa06e4ceb35e65f7eb7d
DanWarrioR <norkinDanya2006@gmail.com>
2024-11-17 19:15:33 +0100

    Moved consts to configs

---
+++ Source/Configs/ColorsConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Source.Configs
+{
+	public class ColorsConfig
+	{
+		public const ConsoleColor PlayerCursorColor = ConsoleColor.Green;
+		public const ConsoleColor DestroyedShipColor = ConsoleColor.Red;
+		public const ConsoleColor MissShotColor = ConsoleColor.Gray;
+		public const ConsoleColor SeaColor = ConsoleColor.Gray;
+		public const ConsoleColor ShipColor = ConsoleColor.DarkBlue;
+	}
+}
---
+++ Source/Configs/IconsConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Source.Configs
+{
+	public class IconsConfig
+	{
+		public const char PlayerCursorIcon = 'o';
+		public const char SeaIcon = '.';
+		public const char ShipIcon = '⌂';
+		public const char DestroyedShipIcon = 'X';
+		public const char MissShotIcon = '#';
+	}
+}
