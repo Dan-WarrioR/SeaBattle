64ef2a9fe79a2c0459cee150e00f8d47739cdc1f
DanWarrioR <norkinDanya2006@gmail.com>
2024-12-13 23:42:42 +0100

    Created user class for data and player logic

---
+++ Source/Users/User.cs
@@ -0,0 +1,22 @@
+﻿namespace Source.Users
+{
+	public class User
+	{
+		public string Name { get; private set; }
+
+		public int WinCount {  get; private set; }
+
+		public int LosesCount { get; private set; }
+
+		public float WinRate => WinCount + LosesCount == 0 ? 0f : WinCount / (WinCount + LosesCount);
+
+		public Player Player { get; private set; }
+
+		public User(string name, Player player)
+		{
+			Name = name;
+
+			Player = player;
+		}
+	}
+}
