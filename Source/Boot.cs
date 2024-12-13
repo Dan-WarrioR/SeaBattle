ab167923d3b8a28dca5522281676bb6076631fe3
DanWarrioR <norkinDanya2006@gmail.com>
2024-12-13 23:43:32 +0100

    Cleaned Application and boot classes

---
+++ Source/Boot.cs
@@ -0,0 +1,14 @@
+﻿using Source.Core;
+
+namespace Source
+{
+	public class Boot
+	{
+		static void Main(string[] args)
+		{
+			Application application = new();
+
+			application.Launch();
+		}
+	}
+}
---
+++ Source/Core/Application.cs
@@ -0,0 +1,25 @@
+﻿namespace Source.Core
+{
+    public class Application
+    {
+        public void Launch()
+        {
+            InitializeConsoleSettings();
+
+            LaunchGame();
+        }
+
+        private void InitializeConsoleSettings()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.CursorVisible = false;
+        }
+
+        private void LaunchGame()
+        {
+            var game = new Game();
+
+            game.Launch();
+        }
+    }
+}
