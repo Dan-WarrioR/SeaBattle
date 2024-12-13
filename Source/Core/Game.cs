bef67e824f495a43f079a7ec52d3f7448d1a0313
DanWarrioR <norkinDanya2006@gmail.com>
2024-12-13 23:51:57 +0100

    Updated game core for new standarts

---
+++ Source/Core/Game.cs
@@ -0,0 +1,81 @@
+﻿using Source.ProfileSystem;
+using Source.Users;
+
+namespace Source.Core
+{
+	public class Game
+	{
+		private User _user1;
+		private User _user2;
+
+		private int _player1Score = 0;
+		private int _player2Score = 0;
+
+		private ProfileLoader _profileLoader;
+
+		public void Launch()
+		{
+			SetupProfiles();
+
+			while (!IsEndGame())
+			{
+				Round round = new(_user1.Player, _user2.Player);
+
+				round.PlayRound();
+
+				ProcessRoundResult(round.RoundResult);
+
+				DrawRoundScore();
+			}
+		}
+
+		private void SetupProfiles()
+		{
+			_profileLoader = new();
+
+			var profileSelector = new ProfileSelector(_profileLoader);
+
+			Console.Clear();
+			Console.WriteLine("Choose 1 profile!");
+
+			List<int> reservedNumbers = new();
+
+			_user1 = profileSelector.SelectProfile(reservedNumbers);
+
+			Console.Clear();
+			Console.WriteLine("Choose 2 profile!");
+
+			_user2 = profileSelector.SelectProfile(reservedNumbers);
+		}
+
+		private void ProcessRoundResult(RoundResult result)
+		{
+			if (result == RoundResult.Player1Win)
+			{
+				_player1Score++;
+
+				return;
+			}
+
+			_player2Score++;
+		}
+
+		
+
+		private bool IsEndGame()
+		{
+			return _player1Score >= 3 || _player2Score >= 3;
+		}
+
+		
+
+		private void DrawRoundScore()
+		{
+			Console.Clear();
+
+			Console.WriteLine($"\tCurrent Score: {_player1Score} | {_player2Score}");
+
+			Thread.Sleep(5000);
+		}
+	}
+}
---
+++ Source/Core/Round.cs
@@ -0,0 +1,144 @@
+﻿using Source.MapGeneration;
+using Source.Rendering;
+using Source.Users;
+using Source.Abilities;
+
+namespace Source.Core
+{
+    public enum GameMode
+    {
+        PVE,
+        EVE,
+        PVP,
+    }
+
+	public enum RoundResult
+	{
+		Player1Win,
+		Player2Win,
+	}
+
+	public class Round
+    {
+        public RoundResult RoundResult { get; private set; }  
+
+        private Renderer _renderer;
+
+        private Player _player1;
+        private Player _player2;
+        private Player _currentPlayer;
+
+        private bool _onePlayerLostAllShips = false;
+        private bool _isShipBombed = false;
+
+        private List<BaseAbility> _activeAbilities = new();
+
+        public Round(Player player1, Player player2)
+        {
+			_player1 = player1;
+			_player2 = player2;
+
+			_player1.Map.OnCellBombed += OnBombedCell;
+			_player2.Map.OnCellBombed += OnBombedCell;
+
+			_renderer = new(_player1, _player2);
+
+			_currentPlayer = _player1;
+		}
+
+		public void PlayRound()
+        {
+			Draw();
+
+            while (!IsRoundEnd())
+            {
+                CalculateInput();
+                ProcessTurn();
+                Draw();
+            }
+
+            EndRound();
+		}     
+
+        private void CalculateInput()
+        {
+            _currentPlayer.CalculateInput();
+        }
+
+        private void ProcessTurn()
+        {
+            ResetPreviousActiveAbilities();
+
+            ProcessAbility();
+
+            if (!_currentPlayer.IsConfirmed)
+            {
+                return;
+            }
+
+            _currentPlayer.TryBombCell();
+
+            if (!_isShipBombed)
+            {
+                SwitchTurn();
+
+                _isShipBombed = false;
+            }
+
+            _currentPlayer.ResetCurrentAbility();
+        }
+
+        private void ProcessAbility()
+        {
+            var ability = _currentPlayer.GetSelectedAbility();
+
+            if (ability != null)
+            {
+                ability.Apply(_currentPlayer.CurrentPosition);
+
+                _activeAbilities.Add(ability);
+            }
+        }
+
+        private void ResetPreviousActiveAbilities()
+        {
+            foreach (var ability in _activeAbilities)
+            {
+                ability.Reset();
+            }
+
+            _activeAbilities.Clear();
+        }
+
+        private void SwitchTurn()
+        {
+            _currentPlayer = _currentPlayer == _player1 ? _player2 : _player1;
+        }
+
+        private void OnBombedCell(Cell cell)
+        {
+            _isShipBombed = cell.IsShip;
+
+            _onePlayerLostAllShips = _player1.ShipsCount <= 0 || _player2.ShipsCount <= 0;
+        }
+
+        private void EndRound()
+        {
+            RoundResult = _player1.ShipsCount <= 0 ? RoundResult.Player1Win : RoundResult.Player2Win;
+		}
+
+
+
+        private bool IsRoundEnd()
+        {
+            return _onePlayerLostAllShips;
+        }
+
+
+
+        private void Draw()
+        {
+            _renderer.Draw();
+        }
+    }
+}
